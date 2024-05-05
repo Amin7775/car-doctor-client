a23c4dd75f2534129a10db5e65a9e08978769d59
Amin7775 <amin.dpi24@gmail.com>
2024-05-05 19:17:33 +0600

    Load Services Fake Data And Display Service Card

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import AboutUs from "./HomeComponents/AboutUs";
 import Banner from "./HomeComponents/Banner";
+import Services from "./HomeComponents/Services/Services";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <AboutUs></AboutUs>
+            <Services></Services>
             This is home
         </div>
     );
