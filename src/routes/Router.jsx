fa153497b0aabdfaf878c1bbadffe4b4efa0e0a2
Amin7775 <amin.dpi24@gmail.com>
2024-05-05 14:56:59 +0600

    Added react router dom routes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,47 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/About/About";
+import Services from "../Pages/Services/Services";
+import Blog from "../Pages/Blog/Blog";
+import Contact from "../Pages/Contact/Contact";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <Layout></Layout>,
+      children:[
+        {
+          path:'/',
+          element: <Home></Home>
+        },
+        {
+          path:'/about',
+          element: <About></About>
+        },
+        {
+          path:'/services',
+          element: <Services></Services>
+        },
+        {
+          path: '/blog',
+          element:<Blog></Blog>
+        },
+        {
+          path: '/contact',
+          element: <Contact></Contact>
+        },
+        {
+          path: '/signin',
+          element: <Login></Login>
+        },
+        {
+          path: '/signup',
+          element: <Register></Register>
+        }
+      ]
     },
   ]);
 
